3f844c1e5f8ea17649dde7168ead15c3820c084b
Maarten van der Velden <maarten.van.der.velden@philips.com>
2016-09-20 16:23:40 +0200

    Added pods for integration test target

---
+++ BDDDemo/Pods/Target Support Files/Pods-BDDDemoIntegrationTests/Pods-BDDDemoIntegrationTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BDDDemoIntegrationTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BDDDemoIntegrationTestsVersionString[];
+
