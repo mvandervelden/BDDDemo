17347fb630f4b5bd035a1d3b2984a078db5b0a2f
Maarten van der Velden <maarten.van.der.velden@philips.com>
2016-09-22 16:01:26 +0200

    Base implementation for steps file

--- Source/BDDDemoIntegrationTests/StepDefinitions.swift
+++ Source/BDDDemoIntegrationTests/StepDefinitions.swift
@@ -6,8 +6,26 @@ import KIF
 class StepDefinitions: NSObject {
     func setup() {
         
-        Given("I am on the master screen") { (args, userInfo) -> Void in
+        Given("the \"(.*)?\" screen is displayed") { (args, userInfo) -> Void in
+            expect(true).to(beFalse(), description: "Implementation Pending")
         }
+        
+        MatchAll("I tap the \"(.*)?\" button") { (args, userInfo) -> Void in
+            expect(true).to(beFalse(), description: "Implementation Pending")
+        }
+        
+        And("I enter title \"(.*)?\"") { (args, userInfo) -> Void in
+            expect(true).to(beFalse(), description: "Implementation Pending")
+        }
+
+        Then("item \"(.*)?\" is saved on the \"(.*)?\" screen") { (args, userInfo) -> Void in
+            expect(true).to(beFalse(), description: "Implementation Pending")
+        }
+        
+        Then("the \"save\" button is disabled") { (args, userInfo) -> Void in
+            expect(true).to(beFalse(), description: "Implementation Pending")   
+        }
+        
     }
     
     fileprivate func tester(_ file: String = #file, _ line: Int = #line) -> KIFUITestActor {
